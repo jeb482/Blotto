8cde58179996eb194f43425e207c443643af99ec
dylanjohnke <dylanjohnke@gmail.com>
2017-05-06 01:35:36 -0400

    Update blotto.py
    added numSoldiers, respond() and level k response

--- blotto.py
+++ blotto.py
@@ -42,3 +42,53 @@ def best_response_one_day(enemy_strategy, num_soldiers=None):
         current_soldiers -= (bf+1)
     ######
     
+    
+    
+    
+def numSoldiers(strategy):
+    n = 0
+    for i in range(0, len(strategy)):
+        n += strategy[i]
+    return n
+
+def respond(player_strategy, enemy_strategy):
+    altMove = best_response_one_day(enemy_strategy, numSoldiers(player_strategy)-1)
+    curScore = run_blotto_game(player_strategy, enemy_strategy)
+    score = run_blotto_game(altMove, enemy_strategy)
+    if (score[0] - score[2] > curScore[0] - curScore[2]):
+        return altMove
+    else:
+        return player_strategy
+    
+  
+
+#very brute force level k response, currently only works for 4 battlefields
+def levelkResponse(player_strategy, enemy_strategy, k):
+    if (k <= 0):
+        return respond(player_strategy, enemy_strategy)
+    if (k == 1):
+        n = numSoldiers(player_strategy)-1
+        bestStrat = player_strategy
+        bestScore = run_blotto_game(bestStrat, respond(enemy_strategy, bestStrat))
+        for i in range(0, n+1):
+            for j in range(0, n+1-i):
+                for l in range(0, n+1-i-j):
+                    strat = [i, j, l, n-i-j-l]
+                    score = run_blotto_game(strat, respond(enemy_strategy, strat))
+                    if (score[0] - score[2] > bestScore[0] - bestScore[2]):
+                        bestStrat = strat
+                        bestScore = score
+        return bestStrat
+    #need better way to do this without brute force    
+    #maybe start with total brute force and then try to notice a pattern?
+    if (k > 1):
+        return respond(player_strategy, levelkResponse(enemy_strategy, player_strategy, k-1))
+    
+#Question: can you ever have a scenario where, when the curtain is lifted, both players would like to move first?
+#i.e. the loser would like to mitigate their losses and the winner would like to pre-empt that?
+#Statement: at any point in time, if you're going to switch moves, you might as well switch to your current best response
+#True or false?
+#Idea: number of troops left at end counts towards utility? Either a significant amount or just as a tiebreaker
+#becomes more similar to an auction when arbitrary number of people in war
+    
+    
